1cd3f833f10e7b3c2a8078c78690f2208c3388ad
Garavirod <rodrigogarciaavila26@gmail.com>
2021-05-27 19:26:34 -0500

    Author Instances were created using Factories and seedrs

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Laravel\Lumen\Auth\Authorizable;
+
 
 class Author extends Model{
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        // How many instamnces you want to create
+        Author::factory()->count(50)->create();
     }
 }
